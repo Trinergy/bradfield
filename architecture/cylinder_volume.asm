d920148e24f3a5bfa8970bcbb3127b625d7bba5d
Kenny Wu <wu.kenny1221@gmail.com>
2019-02-10 17:04:34 -0800

    Architecture - MIPS volume of cylinder

---
+++ architecture/cylinder_volume.asm
@@ -0,0 +1,14 @@
+# V = pi * r^2 * h
+main:
+li.s $f0, 3.14 # pi
+li.s $f1, 10.0 # radius
+li.s $f2, 5.0 # height
+
+mul.s $f3, $f1, $f1 # r^2
+
+mul.s $f4, $f2, $f3  # r^2h
+
+mul.s $f12, $f0, $f4 # pi * r^2h
+
+li $v0, 2
+syscall
