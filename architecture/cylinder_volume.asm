b5b3fc6ea0ac1cb13e75600226bb8aaf4e052c7b
Kenny Wu <wu.kenny1221@gmail.com>
2019-02-10 17:11:06 -0800

    Architecture - fix comments

--- architecture/cylinder_volume.asm
+++ architecture/cylinder_volume.asm
@@ -6,9 +6,9 @@ li.s $f2, 5.0 # height
 
 mul.s $f3, $f1, $f1 # r^2
 
-mul.s $f4, $f2, $f3  # r^2h
+mul.s $f4, $f2, $f3  # r^2 * h
 
-mul.s $f12, $f0, $f4 # pi * r^2h
+mul.s $f12, $f0, $f4 # pi * r^2 * h
 
 li $v0, 2
 syscall
